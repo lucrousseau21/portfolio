0cbc6ff615abac1541fff1eb8521667109b3f5bf
lucrousseau <luc.rousseaupro@gmail.com>
2024-10-17 11:55:03 +0200

    0.1

---
+++ app/_components/Acceuil.tsx
@@ -0,0 +1,25 @@
+import { Section } from "./Section"
+
+export const Acceuil = () => {
+    return (
+    <Section className="flex max-lg:flex-col items-start">
+        <div className="flex-[2]">
+            <h2>
+                Luc Rousseau
+            </h2>
+            <h3>
+                Student Web Developer
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae veritatis nihil harum sapiente perspiciatis reiciendis temporibus, fuga error delectus iusto corrupti aperiam, inventore, repudiandae obcaecati necessitatibus deserunt magnam nemo laboriosam!
+            </p>
+        </div>
+        <div className="flex-1">
+            <img 
+                src="http://luc-rousseau.com/media/portrait1.png" 
+                alt="image luc rousseau" 
+                className="w-full h-auto" 
+            />
+        </div>
+    </Section>
+)}
---
+++ app/_components/Header.tsx
@@ -0,0 +1,33 @@
+import { Button, buttonVariants } from "@/components/ui/button"
+import { GithubIcon } from "./icons/GithubIcon"
+import { Section } from "./Section"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { LinkedinIcon } from "./icons/LinkedinIcon"
+
+export const Header = () => {
+    return (
+        <header className="sticky top-0 py-4">
+            <Section className="flex items-baseline">
+                <h1 className="text-lg font-bold text-primary">
+                    luc-rousseau.com
+                </h1>
+                <div className="flex-1"/>
+                <ul className="flex items-center gap-2">
+                    <Link
+                        href="https://github.com/lucrousseau21" 
+                        className={cn(buttonVariants({variant: "outline"}), "size-6")} 
+                        target="_blank">
+                            <GithubIcon size={16} className="text-foreground"/>
+                    </Link>
+                    <Link
+                        href="https://www.linkedin.com/in/luc-rousseau/" 
+                        className={cn(buttonVariants({variant: "outline"}), "size-6")} 
+                        target="_blank">
+                            <LinkedinIcon size={16} className="text-foreground"/>
+                    </Link>
+                </ul>
+            </Section>
+        </header>
+    )
+}
