7a449123a77cd04d1a3e3de7bf9b803681eb373c
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-11-02 11:24:04 +0100

    début d'ajout de card

---
+++ app/_components/StatusSection.tsx
@@ -0,0 +1,13 @@
+import { Section } from "./Section";
+
+export const Status = () => {
+  return (
+    <Section className="flex max-lg:flex-col items-start">
+      <div className="flex-[2]">Side project</div>
+      <div className="flex-1">
+        <div>work</div>
+        <div>contact me</div>
+      </div>
+    </Section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Header } from './_components/Header';
 import { Acceuil } from "./_components/Acceuil";
 import { Spacing } from "./_components/Spacing";
+import { Status } from "./_components/StatusSection";
 
 export default function Home() {
   return (
@@ -11,6 +12,10 @@ export default function Home() {
       <Spacing size="md" />
 
       <Acceuil />
+
+      <Spacing size="md" />
+
+      <Status />
     </main>
   );
 }
