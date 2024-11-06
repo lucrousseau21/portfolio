2aac2dccfe61a19ece55917525941f151959b21c
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-11-06 18:42:46 +0100

    ajout1 logo lucrousseau

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,14 +4,16 @@ import { Section } from "./Section"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
 import { LinkedinIcon } from "./icons/LinkedinIcon"
+import { LucRousseauLogo } from "./icons/LucRousseauLogo"
 
 export const Header = () => {
     return (
-        <header className="sticky top-0 py-4 backdrop-blur-xl bg-white/60">
+        <header className="sticky top-0 py-4 backdrop-blur-xl bg-background/60 ">
             <Section className="flex items-baseline">
                 <h1 className="text-lg font-bold text-primary">
                     luc-rousseau.com
                 </h1>
+                <LucRousseauLogo size={250} />
                 <div className="flex-1"/>
                 <ul className="flex items-center gap-2">
                     <Link
