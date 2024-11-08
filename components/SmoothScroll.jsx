4d5284e21ebdda4ef498b2c4ce17cb38c3edcdb2
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-11-09 00:20:50 +0100

    mise en place de lenis pour production

--- components/SmoothScroll.jsx
+++ components/SmoothScroll.jsx
@@ -1,66 +1,66 @@
-// "use client";
-// import { useEffect, useRef } from "react";
-// import Lenis from "lenis";
-
-// function SmoothScroll({ children }) {
-//   const lenisRef = useRef(null);
-
-//   useEffect(() => {
-//     const lenis = new Lenis({
-//       lerp: 0.1,
-//       duration: 1.5,
-//       smoothTouch: true,
-//     });
-
-//     lenisRef.current = lenis;
-
-//     function raf(time) {
-//       lenis.raf(time);
-//       requestAnimationFrame(raf);
-//     }
-
-//     requestAnimationFrame(raf);
-
-//     return () => {
-//       lenis.destroy();
-//     };
-//   }, []);
-
-//   return <div ref={lenisRef}>{children}</div>;
-// }
-
-// export default SmoothScroll;
-
 "use client";
 import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
+import Lenis from "lenis";
 
 function SmoothScroll({ children }) {
-  const containerRef = useRef(null);
+  const lenisRef = useRef(null);
 
   useEffect(() => {
-    const container = containerRef.current;
-
-    gsap.to(container, {
-      scrollTrigger: {
-        trigger: container,
-        start: "top top",
-        end: "bottom bottom",
-        scrub: true,
-        markers: true,
-      },
-      y: -100,
+    const lenis = new Lenis({
+      lerp: 0.1,
+      duration: 1,
+      smoothTouch: true,
     });
 
+    lenisRef.current = lenis;
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
     return () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+      lenis.destroy();
     };
   }, []);
 
-  return <div ref={containerRef}>{children}</div>;
+  return <div ref={lenisRef}>{children}</div>;
 }
 
 export default SmoothScroll;
+
+// "use client";
+// import { useEffect, useRef } from "react";
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// function SmoothScroll({ children }) {
+//   const containerRef = useRef(null);
+
+//   useEffect(() => {
+//     const container = containerRef.current;
+
+//     gsap.to(container, {
+//       scrollTrigger: {
+//         trigger: container,
+//         start: "top top",
+//         end: "bottom bottom",
+//         scrub: true,
+//         markers: true,
+//       },
+//       y: -100,
+//     });
+
+//     return () => {
+//       ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+//     };
+//   }, []);
+
+//   return <div ref={containerRef}>{children}</div>;
+// }
+
+// export default SmoothScroll;
