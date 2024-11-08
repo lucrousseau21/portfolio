c9780e463e26628e5fe21867a39596ccb68e70fc
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-11-08 18:16:11 +0100

    z

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+const nextConfig = {};
 
-// export default nextConfig;
+export default nextConfig;
 
-export default {
-    output: 'export',
-  };
+// export default {
+//     output: 'export',
+//   };
